bad7bbbe77a027fc27f4f483d4ed5d78b40946e8
safin33221 <safin33221@gmail.com>
2026-01-23 03:17:58 +0600

    fix prisma adapter + esm imports

--- dist/lib/prisma.js
+++ dist/lib/prisma.js
@@ -1,15 +1,7 @@
-// import "dotenv/config";
-// import { PrismaPg } from '@prisma/adapter-pg'
-// import { PrismaClient } from "../../generated/prisma/client";
-// // import { PrismaClient } from '../generated/prisma/client'
-// const connectionString = `${process.env.DATABASE_URL}`
-// const adapter = new PrismaPg({ connectionString })
-// const prisma = new PrismaClient({ adapter })
-// export { prisma }
-//-------------------------------------------------------------------------------------
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
+import pkg from 'pg';
+const { Pool } = pkg;
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
@@ -18,4 +10,15 @@ const prisma = new PrismaClient({
     adapter,
 });
 export default prisma;
+// import { PrismaClient } from '@prisma/client';
+// import { PrismaPg } from '@prisma/adapter-pg';
+// import { Pool } from 'pg';
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+// });
+// const adapter = new PrismaPg(pool);
+// const prisma = new PrismaClient({
+//     adapter,
+// });
+// export default prisma;
 //# sourceMappingURL=prisma.js.map
--- dist/server.js
+++ dist/server.js
@@ -1,14 +1,14 @@
 import dotenv from 'dotenv';
-import prisma from './lib/prisma';
-import app from './app';
 dotenv.config();
+import prisma from './lib/prisma.js';
+import app from './app.js';
 const PORT = process.env.PORT || 5000;
 async function bootstrap() {
     try {
         await prisma.$connect();
         console.log('🟢 Database connected');
         app.listen(PORT, () => {
-            console.log(`🚀 Server running on http://localhost:${PORT}`);
+            console.log(`🚀 Server running on port ${PORT}`);
         });
     }
     catch (error) {
